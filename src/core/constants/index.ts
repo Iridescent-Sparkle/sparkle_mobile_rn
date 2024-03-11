c37b2a8badbb66c4c998f163d00b6255ec52014e
iridescent <2389504513@qq.com>
2024-03-11 23:05:28 +0800

    🐳 chore(依赖): 集成图标库

---
+++ src/core/constants/index.ts
@@ -0,0 +1,186 @@
+export const IMAGE_PREFIX = 'https://sparkle-cdn.oss-cn-chengdu.aliyuncs.com/sparkle-mobile'
+export const workTypeList = [
+  {
+    key: '1',
+    label: '现场(办公室办公)',
+  },
+  {
+    key: '2',
+    label: '远程(在家工作)',
+  },
+]
+
+export const jobLevelList = [
+  {
+    key: '1',
+    label: '实习/ OJT',
+  },
+  {
+    key: '2',
+    label: '初级/初级，学徒',
+  },
+  {
+    key: '3',
+    label: '助理/主管',
+  },
+  {
+    key: '4',
+    label: '中高层/经理',
+  },
+  {
+    key: '5',
+    label: '董事/行政人员',
+  },
+]
+
+export const employmentTypeList = [
+  {
+    key: '1',
+    label: '全职',
+  },
+  {
+    key: '2',
+    label: '兼职',
+  },
+  {
+    key: '3',
+    label: '自由',
+  },
+  {
+    key: '4',
+    label: '合同',
+  },
+]
+
+export const experienceList = [
+  {
+    key: '1',
+    label: '没有经验',
+  },
+  {
+    key: '2',
+    label: '1 - 5年',
+  },
+  {
+    key: '3',
+    label: '6 - 10年',
+  },
+  {
+    key: '4',
+    label: '10年以上',
+  },
+]
+
+export const educationList = [
+  {
+    key: '1',
+    label: '高中以下',
+  },
+  {
+    key: '2',
+    label: '高中',
+  },
+  {
+    key: '3',
+    label: '副学士',
+  },
+  {
+    key: '4',
+    label: '学士学位',
+  },
+  {
+    key: '5',
+    label: '硕士学位',
+  },
+  {
+    key: '6',
+    label: '博士或专业学位',
+  },
+]
+
+export const jobFunctionList = [
+  {
+    key: '1',
+    label: '会计与金融',
+  },
+  {
+    key: '2',
+    label: '行政和协调',
+  },
+  {
+    key: '3',
+    label: '建筑与工程',
+  },
+  {
+    key: '4',
+    label: '艺术与体育',
+  },
+  {
+    key: '5',
+    label: '客户服务',
+  },
+  {
+    key: '6',
+    label: '教育及培训',
+  },
+  {
+    key: '7',
+    label: '总务',
+  },
+  {
+    key: '8',
+    label: '卫生及医疗',
+  },
+  {
+    key: '9',
+    label: '酒店及旅游业',
+  },
+  {
+    key: '10',
+    label: '人力资源',
+  },
+  {
+    key: '11',
+    label: '资讯科技及软件',
+  },
+  {
+    key: '12',
+    label: '法律',
+  },
+  {
+    key: '13',
+    label: '管理及顾问',
+  },
+  {
+    key: '14',
+    label: '制造与生产',
+  },
+  {
+    key: '15',
+    label: '媒体及创意',
+  },
+  {
+    key: '16',
+    label: '公共服务及非政府机构',
+  },
+  {
+    key: '17',
+    label: '安全及保安',
+  },
+  {
+    key: '18',
+    label: '销售及市场推广',
+  },
+  {
+    key: '19',
+    label: '科学',
+  },
+  {
+    key: '20',
+    label: '供应链',
+  },
+  {
+    key: '21',
+    label: '写作与内容',
+  },
+]
