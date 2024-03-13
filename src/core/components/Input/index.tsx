3e75e999ea3fa27f901c302a4ed7f7f585de3d64
iridescent <2389504513@qq.com>
2024-03-13 23:03:47 +0800

    ✨ feat: 添加输入框

---
+++ src/core/components/Input/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+interface Props {}
+
+function Input(props: Props) {
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+export default Input
