dba3fe259149d41469f08bfbcf3b90959b0532a1
iridescent <2389504513@qq.com>
2024-04-08 01:13:22 +0800

    ✨ feat: 完善修改职位和积分记录

--- src/page/boss/integral-list/index.tsx
+++ src/page/boss/integral-list/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@fruits-chain/react-native-xiaoshu'
 import { StackActions, useFocusEffect, useNavigation } from '@react-navigation/native'
 import React, { useCallback, useState } from 'react'
 import { FlatList, Text, View } from 'react-native'
+import type { barDataItem, pieDataItem } from 'react-native-gifted-charts'
 import { BarChart, PieChart } from 'react-native-gifted-charts'
 import ConsumeListCard from './components/consume-list-card'
 import { themeColor } from '@/core/styleSheet/themeColor'
@@ -13,12 +14,28 @@ export default function IntegralList() {
   const userStore = useUserStore()
   const navigation = useNavigation()
   const [consumeList, setConsumeList] = useState([] as IntegralRecord[])
+
+  const [pieData, setPieData] = useState([] as pieDataItem[])
+  const [barData, setBarData] = useState([] as barDataItem[])
+  const [selectPie, setSelectPie] = useState({})
   const getInitData = async () => {
     try {
-      const { data: consumeListData } = await request.post({}, {
-        url: `/boss/consume/user`,
-      })
+      const [{ data: consumeListData }, { data: pieData }, { data: barData }] = await Promise.all([
+        request.post({}, {
+          url: `/boss/consume/user`,
+        }),
+        request.post({}, {
+          url: `/boss/consume/usage/type`,
+        }),
+        request.post({}, {
+          url: `/boss/consume/usage/days`,
+        }),
+      ])
       setConsumeList(consumeListData)
+      const formatPieData = pieData.map((item: any) => ({ value: Number(item.totalIntegral), color: PIE_DATA_MAP[item.type].color, text: PIE_DATA_MAP[item.type].label }))
+      setPieData(formatPieData)
+      setBarData(barData.map((item: any) => ({ ...item, frontColor: '#177AD5' })))
+      setSelectPie(formatPieData[0])
     }
     catch (error) {
 
@@ -32,21 +49,71 @@ export default function IntegralList() {
     navigation.dispatch(StackActions.replace('RechargeIntegral'))
   }
 
-  const pieData = [
-    { value: 54, color: '#177AD5' },
-    { value: 40, color: '#79D2DE' },
-    { value: 20, color: '#ED6665' },
-  ]
+  const PIE_DATA_MAP = {
+    publish: { color: '#006DFF', label: '发布职位' },
+    chat: { color: '#79D2DE', label: '发起聊天' },
+    recharge: { color: '#ED6665', label: '充值积分' },
+  } as Record<string, any>
 
-  const barData = [
-    { value: 250, label: 'M' },
-    { value: 500, label: 'T', frontColor: '#177AD5' },
-    { value: 745, label: 'W', frontColor: '#177AD5' },
-    { value: 320, label: 'T' },
-    { value: 600, label: 'F', frontColor: '#177AD5' },
-    { value: 256, label: 'S' },
-    { value: 300, label: 'S' },
-  ]
+  const pieTotal = pieData.reduce((acc, cur) => acc + cur.value, 0)
+
+  const renderDot = (color) => {
+    return (
+      <View
+        style={{
+          height: 10,
+          width: 10,
+          borderRadius: 5,
+          backgroundColor: color,
+          marginRight: 10,
+        }}
+      />
+    )
+  }
+  const renderLegendComponent = () => {
+    return (
+      <>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: 10,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              width: 120,
+              marginRight: 20,
+            }}
+          >
+            {renderDot('#006DFF')}
+            <Text style={{ color: themeColor.black85 }}>发布职位</Text>
+          </View>
+          <View
+            style={{ flexDirection: 'row', alignItems: 'center', width: 120 }}
+          >
+            {renderDot('#79D2DE')}
+            <Text style={{ color: themeColor.black85 }}>发起聊天</Text>
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              width: 120,
+              marginRight: 20,
+            }}
+          >
+            {renderDot('#ED6665')}
+            <Text style={{ color: themeColor.black85 }}>充值积分</Text>
+          </View>
+        </View>
+      </>
+    )
+  }
   return (
     <FlatList
       style={styles.container}
@@ -63,30 +130,58 @@ export default function IntegralList() {
             <Button onPress={handleRechargeClick} type="hazy" style={styles.headerButton} size="s">去充值</Button>
           </View>
           {/* 饼图 */}
-          <View style={styles.pieWrapper}>
-            <PieChart
-              data={pieData}
-              showText
-              textColor="black"
-              radius={pxToDp(200)}
-              textSize={pxToDp(24)}
-              focusOnPress
-              showValuesAsLabels
-              showTextBackground
-              textBackgroundRadius={pxToDp(26)}
-            />
-          </View>
+          {
+            pieData && (
+              <View style={styles.pieWrapper}>
+                <Text style={styles.title}>积分使用分布情况</Text>
+                <PieChart
+                  data={pieData}
+                  textColor="black"
+                  radius={pxToDp(200)}
+                  textSize={pxToDp(24)}
+                  showValuesAsLabels
+                  showTextBackground
+                  onPress={(value) => { setSelectPie(value) }}
+                  centerLabelComponent={() => {
+                    return (
+                      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                        <Text
+                          style={{ fontSize: 22, color: themeColor.black85, fontWeight: 'bold' }}
+                        >
+                          {`${Number(selectPie.value) / Number(pieTotal) * 100}%`}
+                        </Text>
+                        <Text style={{ fontSize: 14, color: themeColor.black85 }}>{selectPie.text}</Text>
+                      </View>
+                    )
+                  }}
+                  textBackgroundRadius={pxToDp(26)}
+                />
+                <View style={{ marginTop: 20 }}>
+                  {renderLegendComponent()}
+                </View>
+              </View>
+            )
+          }
           {/* 折线图 */}
-          <View style={styles.barWrapper}>
-            <BarChart
-              barWidth={pxToDp(36)}
-              barBorderRadius={pxToDp(24)}
-              frontColor="lightgray"
-              data={barData}
-              yAxisThickness={0}
-              xAxisThickness={0}
-            />
-          </View>
+          {
+            barData && (
+              <View style={styles.barWrapper}>
+                <Text style={styles.title}>近七日积分使用情况</Text>
+                <BarChart
+                  barWidth={pxToDp(36)}
+                  barBorderRadius={pxToDp(24)}
+                  frontColor="lightgray"
+                  data={barData}
+                  showXAxisIndices
+                  showYAxisIndices
+                  yAxisTextStyle={{ color: 'black' }}
+                  xAxisLabelTextStyle={{ color: 'black' }}
+                  yAxisThickness={0}
+                  xAxisThickness={0}
+                />
+              </View>
+            )
+          }
         </>
       )}
       data={consumeList}
@@ -123,6 +218,12 @@ const styles = create({
   },
   headerButton: {
     borderRadius: 48,
+  },
+  title: {
+    color: themeColor.black85,
+    fontWeight: '700',
+    fontSize: 32,
+    marginBottom: 24,
   },
   pieWrapper: {
     borderRadius: 16,
--- src/page/boss/publish-job/index.tsx
+++ src/page/boss/publish-job/index.tsx
@@ -2,7 +2,7 @@ import { Button, Dialog, Toast } from '@fruits-chain/react-native-xiaoshu'
 import { ScrollView, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useEffect } from 'react'
-import { StackActions, useNavigation } from '@react-navigation/native'
+import { StackActions, useNavigation, useRoute } from '@react-navigation/native'
 import { useUserStore } from '../../../store/user/index'
 import Form from '@/core/components/Form'
 import Input from '@/core/components/Input'
@@ -18,7 +18,7 @@ export default function PublishJob() {
   const insets = useSafeAreaInsets()
   const userStore = useUserStore()
   const form = Form.useForm()
-
+  const route = useRoute<{ key: any, name: any, params: { jobId: string } }>()
   const navigation = useNavigation()
 
   const jobStore = useJobStore()
@@ -28,6 +28,22 @@ export default function PublishJob() {
 
     const minSalary = values.salary[0]
     const maxSalary = values.salary[1]
+    if (route.params.jobId) {
+      await request.post({
+        ...values,
+        minSalary,
+        maxSalary,
+        companyId: userStore.userInfo.companyId,
+      }, { url: `boss/job/${route.params.jobId}` })
+
+      await Toast.success({
+        message: '修改成功',
+        duration: 200,
+        onClose: navigation.goBack,
+      })
+      return
+    }
+
     if (Number(userStore.userInfo.integral) < 1) {
       Dialog.confirm({
         title: '积分不足',
@@ -70,27 +86,37 @@ export default function PublishJob() {
     }
   }
 
+  const getInitData = async () => {
+    try {
+      if (route.params?.jobId) {
+        userStore.getUserInfo()
+        const { data: jobDetail }: { data: JobDetail } = await request.get({}, { url: `boss/job/${route.params.jobId}` })
+        form.setFieldsValue({
+          jobName: jobDetail.jobName,
+          salary: [jobDetail.minSalary, jobDetail.maxSalary],
+          isFullTime: jobDetail.isFullTime,
+          isOnsite: jobDetail.isOnsite,
+          jobExperienceId: jobDetail.jobExperienceId,
+          jobEducationId: jobDetail.jobEducationId,
+          jobLevelId: jobDetail.jobEducationId,
+          jobCategoryId: jobDetail.jobCategoryId,
+          jobDescription: jobDetail.jobDescription,
+          jobRequirements: jobDetail.jobRequirements,
+          address: jobDetail.address,
+          jobBonus: jobDetail.jobBonus.map(item => item.id),
+          headCount: String(jobDetail.headCount),
+          website: jobDetail.website,
+          companyDescription: jobDetail.companyDescription,
+        })
+      }
+    }
+    catch (error) {
+      Toast.fail({ message: '获取职位详情失败' })
+    }
+  }
+
   useEffect(() => {
-    userStore.getUserInfo()
-    form.setFieldsValue({
-      companyDescription: '华为创立于1987年，是全球领先的ICT（信息与通信）基础设施和智能终端提供商，我们致力于把数字世界带入每个人、每个家庭、每个组织，构建万物互联的智能世界：让无处不在的联接，成为人人平等的权利；为世界提供最强算力，让云无处不在，让智能无所不及；所有的行业和组织，因强大的数字平台而变得敏捷、高效、生机勃勃；通过AI重新定义体验，让消费者在家居、办公、出行等全场景获得极致的个性化体验。目前华为约有19.4万员工，业务遍及170多个国家和地区，服务30多亿人口。',
-      educationRequirement: 1,
-      headCount: '2',
-      isFullTime: true,
-      isOnsite: false,
-      jobBonus: [1, 2],
-      jobDescription: 'OD岗是介于外包和正编之间，由第三方公司外企德科签署劳动合同，由华为同正编员工统一管理的一种用工模式，具体如下:薪酬：根据面试结果，涨幅在10-50%不等。人事关系：合同、五险一金由人力资源公司外企德科负责业务关系：人员管理、工作分配、培训考核、绩效评比和晋升由华为负责，并且有华为专门的导师带领。OD定位：定位华为预备役，同德科签署劳动合同，在华为研究所办公，参与华为核心项目研发。拥有完善的新员工培养计划。',
-      jobLevelId: 4,
-      jobLocation: '成都',
-      jobName: '前端开发工程师',
-      jobRequirements: '前端开发工作经验：1.能力突出，工作年限可以放宽。2.精通基本的web前端技术，css，html。 3.熟练掌握bootstrap，angular7-8，jquery vue等前端框架。 4.熟练开发前后台交互（restful），可以作用前端插件实现复杂功能，有网站性能优化，解决兼容性问题等经验。 ',
-      jobCategoryId: 1,
-      salary: [10, 100],
-      website: 'Iridescent.icu',
-      jobExperienceId: 1,
-      jobEducationId: 1,
-      address: '成都',
-    })
+    getInitData()
   }, [])
 
   return (
