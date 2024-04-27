d167eacb55e45259dcb2f3ec287378073b78a678
iridescent <2389504513@qq.com>
2024-04-27 18:07:05 +0800

    🐳 chore: 升级react版本，删除eslint format

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,6 @@ const antfu = require('@antfu/eslint-config').default
 module.exports = antfu({
   react: true,
   typescript: true,
-  formatters: {
-    css: true,
-    html: true,
-    markdown: 'prettier',
-  },
   rules: {
     'ts/no-use-before-define': 'off',
   },
